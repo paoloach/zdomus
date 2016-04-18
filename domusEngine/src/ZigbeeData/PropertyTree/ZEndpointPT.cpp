9b847b173631c7060ac2331bffdf978168206cd3
paoloach <paolo.achdjian@skillbill.it>
2016-04-18 08:18:15 +0200

    Update json format

--- domusEngine/src/ZigbeeData/PropertyTree/ZEndpointPT.cpp
+++ domusEngine/src/ZigbeeData/PropertyTree/ZEndpointPT.cpp
@@ -17,23 +17,23 @@ ZEndpointPT::ZEndpointPT(const ZEndpoint& zEndpoint) noexcept{
 	ptree  inCluster;
 	ptree outCluster;
 
-	add("short address", zEndpoint.getNwkAddr());
-	add("endpoint id", zEndpoint.getEndpoint());
-	add("profile id", zEndpoint.getAppProfId());
-	add("device id", zEndpoint.getAppDeviceId());
-	add("device version", zEndpoint.getAppDevVer());
+	add("short_address", zEndpoint.getNwkAddr());
+	add("endpoint_id", zEndpoint.getEndpoint());
+	add("profile_id", zEndpoint.getAppProfId());
+	add("device_id", zEndpoint.getAppDeviceId());
+	add("device_version", zEndpoint.getAppDevVer());
 	int index=0;
 	for (auto cluster : zEndpoint.getInCluster()) {
 		inCluster.add(boost::lexical_cast<std::string>(index), cluster);
 		index++;
 	}
-	push_back( { "input clusters", inCluster });
+	push_back( { "input_clusters", inCluster });
 	index=0;
 	for (auto cluster : zEndpoint.getOutCluster()) {
 		outCluster.add(boost::lexical_cast<std::string>(index), cluster);
 		index++;
 	}
-	push_back( { "output clusters", outCluster });
+	push_back( { "output_clusters", outCluster });
 }
 
 
