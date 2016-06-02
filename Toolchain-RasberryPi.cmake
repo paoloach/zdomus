0b1a9ab67c46cc5f6af9095d859a0af9e3bf85fc
paoloach <paolo.achdjian@skillbill.it>
2016-06-02 10:20:20 +0200

    fix toolchaing cmake file for rasberry

---
+++ Toolchain-RasberryPi.cmake
@@ -0,0 +1,22 @@
+# this one is important
+SET(CMAKE_SYSTEM_NAME Linux)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   /opt/cross/x-tools6h/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER /opt/cross/x-tools6h/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-g++)
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH  /opt/cross/x-tools6h/arm-unknown-linux-gnueabihf/arm-unknown-linux-gnueabihf/sysroot)
+SET(CMAKE_INSTALL_PREFIX  ${CMAKE_FIND_ROOT_PATH}S/usr/local)
+
+
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+include_directories( /opt/cross/x-tools6h/arm-unknown-linux-gnueabihf/arm-unknown-linux-gnueabihf/sysroot/include)
