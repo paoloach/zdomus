fd13f1dca149db17ff090138109582b0026ef4a6
paoloach <paolo.achdjian@skillbill.it>
2018-02-11 07:18:50 +0100

    DomusEngine: cross compiling cmake script

--- Toolchain-RasberryPi.cmake
+++ Toolchain-RasberryPi.cmake
@@ -3,21 +3,18 @@ SET(CMAKE_SYSTEM_NAME Linux)
 #this one not so much
 SET(CMAKE_SYSTEM_VERSION 1)
 
+SET(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSROOT /opt/cross/x-tools/armv6-unknown-linux-gnueabihf/armv6-unknown-linux-gnueabihf/sysroot)
+set(CMAKE_STAGING_PREFIX /opt/cross/x-tools/armv6-unknown-linux-gnueabihf/armv6-unknown-linux-gnueabihf/sysroot/usr/local)
+
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   /opt/cross/x-tools6h/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-gcc)
-SET(CMAKE_CXX_COMPILER /opt/cross/x-tools6h/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-g++)
-
-# where is the target environment 
-SET(CMAKE_FIND_ROOT_PATH  /opt/cross/x-tools6h/arm-unknown-linux-gnueabihf/arm-unknown-linux-gnueabihf/sysroot)
-SET(CMAKE_INSTALL_PREFIX  ${CMAKE_FIND_ROOT_PATH}/usr/local)
+SET(CMAKE_C_COMPILER   /opt/cross/x-tools/armv6-unknown-linux-gnueabihf/bin/armv6-unknown-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER /opt/cross/x-tools/armv6-unknown-linux-gnueabihf/bin/armv6-unknown-linux-gnueabihf-g++)
 
 
-
-# search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-# for libraries and headers in the target directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 SET(BUILD_V8_VERSION  arm)
 
-include_directories( /opt/cross/x-tools6h/arm-unknown-linux-gnueabihf/arm-unknown-linux-gnueabihf/sysroot/include)
+#include_directories( /opt/cross/x-tools/armv6-unknown-linux-gnueabihf/armv6-unknown-linux-gnueabihf/sysroot/include)
