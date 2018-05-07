87963a1aabb3da5e00ee1206e7fd186d566ae9c0
paoloach <paolo.achdjian@skillbill.it>
2018-05-07 18:06:41 +0200

    add stream operator to LqiResponse

--- zigbee_lib/src/zigbee/messageStructure/LqiResponse.h
+++ zigbee_lib/src/zigbee/messageStructure/LqiResponse.h
@@ -5,6 +5,7 @@
 #ifndef ZIGBEE_LIB_LQIRESPONSE_H
 #define ZIGBEE_LIB_LQIRESPONSE_H
 
+#include <ostream>
 #include <sys/types.h>
 #include "../ExtAddress.h"
 #include "../NwkAddr.h"
@@ -19,7 +20,39 @@ namespace zigbee {
         PreviousChild
     };
 
+    inline std::ostream & operator<<(std::ostream & output, Relationship relationship){
+        switch (relationship){
+            case Relationship::NeighborIsParent:
+                output << "NeighborIsParent";
+                break;
+            case Relationship::NeighborIsChild:
+                output << "NeighborIsChild";
+                break;
+            case Relationship::NeighborIsSibling:
+                output << "NeighborIsSibling";
+                break;
+            case Relationship::NoRelation:
+                output << "NoRelation";
+                break;
+            case Relationship::PreviousChild:
+                output << "PreviousChild";
+                break;
+            default:
+                output << "Undefined";
+                break;
+        }
+        return output;
+    }
+
     struct LqiTable {
+        friend std::ostream &operator<<(std::ostream &os, const LqiTable &table) {
+            os << "panAddr: " << table.panAddr << " ieeeAddr: " << table.ieeeAddr << " nwkAddr: " << table.nwkAddr
+               << " logicalType: " << table.logicalType << " onWhenIdle: " << table.onWhenIdle << " relationship: "
+               << table.relationship << " neighborAcceptJoin: " << table.neighborAcceptJoin << " depth: " << table.depth
+               << " lqi: " << table.lqi << " index: " << table.index;
+            return os;
+        }
+
         ExtAddress panAddr;
         ExtAddress ieeeAddr;
         NwkAddr nwkAddr;
@@ -32,7 +65,17 @@ namespace zigbee {
         uint index;
     };
 
+
     class LqiResponse {
+    public:
+        friend std::ostream &operator<<(std::ostream &os, const LqiResponse &response) {
+            os << "ownerNwkAddr: " << response.ownerNwkAddr << " totalTables: " << response.totalTables << " tables: ";
+            for(auto & lqiTable: response.tables){
+                os << "{" << lqiTable << "}  ";
+            }
+            return os;
+        }
+
     public:
         NwkAddr ownerNwkAddr;
 
